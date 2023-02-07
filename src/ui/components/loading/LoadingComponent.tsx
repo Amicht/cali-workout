cc3b7848daca953fe591ac5d205a2e8cd18dc598
Amicht <vicolicht@gmail.com>
2023-02-07 23:00:07 +0200

    loading component margin center on resize event

--- src/ui/components/loading/LoadingComponent.tsx
+++ src/ui/components/loading/LoadingComponent.tsx
@@ -1,13 +1,22 @@
-import './loading.scss'
+import './loading.scss';
+import React from 'react'
 
 const LoadingComponent = () => {
 
-    const marginTop = (window.innerHeight / 2) - 50;
+  const [marginTop, setMarginTop ] = React.useState((window.innerHeight / 2) - 50);
+
+
+  React.useEffect(() => {
+      window.addEventListener('resize', () => {
+        setMarginTop((window.innerHeight / 2) - 50);
+      });
+  });
+
   return (
     <div className='loading bg-dark'>
         <div 
-            style={{marginTop: marginTop}}
-            className="spinner-border text-secondary spinner-loading" 
+            style={{marginTop}}
+            className="spinner-border text-secondary spinner-loading mx-auto" 
             role="status">
         </div>
     </div>
