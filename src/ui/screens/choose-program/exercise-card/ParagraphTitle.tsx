10c4badc125cf34369064fc1a7df71c291ae55de
Amicht <vicolicht@gmail.com>
2023-02-08 19:14:24 +0200

    paragraph fix

--- src/ui/screens/choose-program/exercise-card/ParagraphTitle.tsx
+++ src/ui/screens/choose-program/exercise-card/ParagraphTitle.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ParagraphTitle = (props:{titleName:string, content:string}) => {
   return (<>
     <h3 className='fw-bold fs-5 text-secondary mt-3'>
-        {props.titleName}:
+        {props.titleName}
     </h3>
     <h4 className={props.titleName === "instructions"? "text-start fs-6":"fs-6"}>{props.content}</h4>
   </>
