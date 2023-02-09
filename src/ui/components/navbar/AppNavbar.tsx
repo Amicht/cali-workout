c012e8a71aa6219f99c3abf0776cdf27e1fbb76d
Amicht <vicolicht@gmail.com>
2023-02-09 20:32:16 +0200

    with logo

--- src/ui/components/navbar/AppNavbar.tsx
+++ src/ui/components/navbar/AppNavbar.tsx
@@ -19,11 +19,19 @@ const AppNavbar = () => {
       variant="dark"  
       expand="md">
       <Container>
-        <Navbar.Brand className='me-5'>CALI-APP</Navbar.Brand>
+        <Navbar.Brand className=''>
+          <Link className='link me-3 mt-1' to="/">
+            <img 
+              alt='web-logo' 
+              src='/web-logo.png'
+              width="170"
+              height="70"
+              className="d-inline-block align-top"/>
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto my-2">
-            <Link className='link me-3 mt-1' to="/">{language.navbar.home}</Link>
             <Link className='link me-3 mt-1' to="/get-started">{language.navbar.get_started}</Link>
             <ChangeLanguageComponent />
           </Nav>
